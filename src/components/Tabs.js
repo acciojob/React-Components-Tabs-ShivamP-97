a713024adf17af1f92d4cf59edfa734738426e89
= <shivampathak19972@gmail.com>
2026-02-21 22:52:32 +0530

    Test

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tabs() {
   return (
-    <div>
+    <>
       <div className="tabs">
         <button id="Home-Tab" className="tab">Home</button>
         <button id="About-Tab" className="tab">About</button>
@@ -12,7 +12,7 @@ function Tabs() {
       <div className="viewport">
         Pages Go Here
       </div>
-    </div>
+    </>
   );
 }
 
